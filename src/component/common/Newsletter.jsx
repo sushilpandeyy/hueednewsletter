b6b86bbfd5ed488db020e4a762f119255d27fe85
Sushil Pandey <contact.sushilpandey@gmail.com>
2025-09-24 01:47:47 +0530

    Fix newsletter form and update site metadata

--- src/component/common/Newsletter.jsx
+++ src/component/common/Newsletter.jsx
@@ -1,63 +1,84 @@
+import { useState } from 'react';
+
 const Newsletter = () => {
+  const [email, setEmail] = useState('');
+  const [status, setStatus] = useState('idle');
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!email) {
+      setStatus('error');
+      setMessage('Please enter a valid email address.');
+      return;
+    }
+
+    setStatus('loading');
+    setMessage('');
+
+    try {
+      const formData = new FormData();
+      formData.append('EMAIL', email);
+      formData.append('b_f039ab124ecbe9e0893d12cc8_91ee2091ac', '');
+
+      const response = await fetch(
+        'https://gmail.us15.list-manage.com/subscribe/post?u=f039ab124ecbe9e0893d12cc8&id=91ee2091ac',
+        {
+          method: 'POST',
+          body: formData,
+          mode: 'no-cors'
+        }
+      );
+
+      setStatus('success');
+      setMessage('Thank you for subscribing!');
+      setEmail('');
+    } catch (error) {
+      setStatus('error');
+      setMessage('Something went wrong. Please try again.');
+    }
+  };
+
   return (
-    <div id="mc_embed_shell">
-      <link href="//cdn-images.mailchimp.com/embedcode/classic-061523.css" rel="stylesheet" type="text/css" />
-      <style type="text/css">
-        {`
-        #mc_embed_signup{background:#fff; false;clear:left; font:14px Helvetica,Arial,sans-serif; width: 600px;}
-        /* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
-           We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
-        `}
-      </style>
-      <div id="mc_embed_signup">
-        <form action="https://gmail.us15.list-manage.com/subscribe/post?u=f039ab124ecbe9e0893d12cc8&amp;id=91ee2091ac" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate">
-          <div id="mc_embed_signup_scroll">
-            <h2>Subscribe</h2>
-            <div className="indicates-required"><span className="asterisk">*</span> indicates required</div>
-            <div className="mc-field-group">
-              <label htmlFor="mce-EMAIL">Email Address <span className="asterisk">*</span></label>
-              <input type="email" name="EMAIL" className="required email" id="mce-EMAIL" required="" defaultValue="" />
-            </div>
-            <div id="mce-responses" className="clear foot">
-              <div className="response" id="mce-error-response" style={{display: 'none'}}></div>
-              <div className="response" id="mce-success-response" style={{display: 'none'}}></div>
-            </div>
-            <div aria-hidden="true" style={{position: 'absolute', left: '-5000px'}}>
-              {/* real people should not fill this in and expect good things - do not remove this or risk form bot signups */}
-              <input type="text" name="b_f039ab124ecbe9e0893d12cc8_91ee2091ac" tabIndex="-1" defaultValue="" />
-            </div>
-            <div className="optionalParent">
-              <div className="clear foot">
-                <input type="submit" name="subscribe" id="mc-embedded-subscribe" className="button" value="Subscribe" />
-                <p style={{margin: '0px auto'}}>
-                  <a href="http://eepurl.com/jn0rYw" title="Mailchimp - email marketing made easy and fun">
-                    <span style={{display: 'inline-block', backgroundColor: 'transparent', borderRadius: '4px'}}>
-                      <img className="refferal_badge" src="https://digitalasset.intuit.com/render/content/dam/intuit/mc-fe/en_us/images/intuit-mc-rewards-text-dark.svg" alt="Intuit Mailchimp" style={{width: '220px', height: '40px', display: 'flex', padding: '2px 0px', justifyContent: 'center', alignItems: 'center'}} />
-                    </span>
-                  </a>
-                </p>
-              </div>
-            </div>
+    <div className="w-full max-w-sm">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-3">
+        <div className="border-b border-black flex items-center justify-between">
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+            className="uppercase bg-transparent outline-none flex-1 py-2"
+            style={{fontFamily:"frankton-mono-bold"}}
+            placeholder="EMAIL"
+            disabled={status === 'loading'}
+          />
+          <button
+            type="submit"
+            disabled={status === 'loading'}
+            className="ml-2 disabled:opacity-50"
+          >
+            <svg width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M0 5L8 5M8 5L4 1M8 5L4 9" stroke="currentColor" strokeWidth="1"/>
+            </svg>
+          </button>
+        </div>
+
+        {message && (
+          <div className={`text-xs uppercase ${status === 'error' ? 'text-red-600' : 'text-green-600'}`} style={{fontFamily:"frankton-mono-bold"}}>
+            {message}
           </div>
-        </form>
-      </div>
-      <script type="text/javascript" src="//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js"></script>
-      <script type="text/javascript" dangerouslySetInnerHTML={{
-        __html: `
-        (function($) {
-          window.fnames = new Array();
-          window.ftypes = new Array();
-          fnames[0]='EMAIL';ftypes[0]='email';
-          fnames[1]='FNAME';ftypes[1]='text';
-          fnames[2]='LNAME';ftypes[2]='text';
-          fnames[3]='ADDRESS';ftypes[3]='address';
-          fnames[4]='PHONE';ftypes[4]='phone';
-          fnames[5]='BIRTHDAY';ftypes[5]='birthday';
-          fnames[6]='COMPANY';ftypes[6]='text';
-        }(jQuery));
-        var $mcj = jQuery.noConflict(true);
-        `
-      }} />
+        )}
+      </form>
+
+      <input
+        type="text"
+        name="b_f039ab124ecbe9e0893d12cc8_91ee2091ac"
+        tabIndex="-1"
+        className="absolute left-[-5000px]"
+        aria-hidden="true"
+      />
     </div>
   );
 };
